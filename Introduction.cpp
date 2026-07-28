13a42609e2eed81cdc337dbfd23203330366cb55
Bishal Dey <bishaldey.in@gmail.com>
2026-07-28 19:43:54 +0530

    day one of code done

---
+++ Introduction.cpp
@@ -0,0 +1,8 @@
+#include <iostream>//iske andar hum cout, cin, endl ka use kar sakte hai jab ye hoga tab hi include kar sakte hai
+using namespace std;//std means standard library of c++ which contains all the classes and functions of c++ standard library
+
+int main(){
+    cout << "Hello Bishal" <<endl;//idhar endl matlab jaise hum enter press karte hai waise hi kaam karega to bring new line and work on that
+    cout << "Hello Bishal" <<endl;//idhar endl matlab jaise hum enter press karte hai waise hi kaam karega to bring new line and work on that
+    return 0;//represents the successful execution/ termination of the program
+}   
