6df103abeb709ca763ac2a8f7c833a7e3e799db4
Bishal Dey <bishaldey.in@gmail.com>
2026-07-29 18:28:03 +0530

    kuch naya sikha regarding user input

---
+++ user_in.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //declaration of variable
+    int marks;
+    cout << "Enter your marks: " << endl;
+
+    
+    cin >> marks;
+    cout << "Marks are :" << marks << endl;
+
+
+
+    return 0;
+}
