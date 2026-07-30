3a7f0cab9a3b1f59a7d10b4adad9b7f5351682b8
Bishal Dey <bishaldey.in@gmail.com>
2026-07-30 07:19:20 +0530

    new changes

--- user_in.cpp
+++ user_in.cpp
@@ -6,8 +6,9 @@ int main(){
     int marks;
     cout << "Enter your marks: " << endl;
 
-    
+    //idhar input liya jata hai 
     cin >> marks;
+    //ye cin output dene k liye hai 
     cout << "Marks are :" << marks << endl;
 
 
