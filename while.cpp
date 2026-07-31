694e15cd430e1dde2a9d2d58d205c59a90260e33
Bishal Dey <bishaldey.in@gmail.com>
2026-07-31 08:32:38 +0530

    new works done

---
+++ while.cpp
@@ -0,0 +1,17 @@
+// while k logic samaj lo
+//while(condition){ idhar initialization hota hai
+// logic    idhar main logic chalta hai loop k
+// updation   idhar update kardo bass khatam
+
+// PRINT 1 TO 5 USING WHILE LOOP
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=10){
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
