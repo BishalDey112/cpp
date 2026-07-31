507be9a9227c611039fd9f8d32026f81195050b3
Bishal Dey <bishaldey.in@gmail.com>
2026-07-31 08:00:57 +0530

    new items

---
+++ break_in_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for(i=0;i<=10;i++){
+        if(i==5){
+            break;// idhar ye break kar deta hai wo wala line
+        }
+        cout << i << endl;
+    }
+}
