39dca6bd978cd60cda97fdbb383965762c29db9e
Bishal Dey <bishaldey.in@gmail.com>
2026-08-01 08:14:23 +0530

    new concepts of loops done

---
+++ do_while.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int i = 1;// this is the initialization
+    do{
+        //logic hai ye niche wala
+        cout << i << " ";
+        //iske baad ye hai updation
+        i = i+1;
+    }while(i<=10); // ye wala hai condition check wala
+
+
+    return 0;
+}
