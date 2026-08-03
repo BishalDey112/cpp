1ba7e2c894c05ff2917ef57a00a4ac00cfc817f8
Bishal Dey <bishaldey.in@gmail.com>
2026-08-03 15:09:53 +0530

    all

--- operators.cpp
+++ operators.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    cout << a << endl;
-    cout << (++a) << endl;//  pre increment pehle increment karo uske baad use karlena
-    cout << (a++) << endl;// increment toh hoga but print next line me hoga increment wala
-    cout << a << endl; //idhar increment wala print hoga
-    cout << (--a) << endl;// idhar its pre decrement same like pre increment
-    cout << (a--) << endl; // same like post decrement
-    cout << a;
+    int a = 5; int b =4;
+    // cout << a << endl;
+    // cout << (++a) << endl;//  pre increment pehle increment karo uske baad use karlena
+    // cout << (a++) << endl;// increment toh hoga but print next line me hoga increment wala
+    // cout << a << endl; //idhar increment wala print hoga
+    // cout << (--a) << endl;// idhar its pre decrement same like pre increment
+    // cout << (a--) << endl; // same like post decrement
+    // cout << a;
 
+    cout << (a&b) << endl;
     return 0;
 }
