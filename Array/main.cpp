70b36e83cb3a910ca5157d8c22ef0197cc5dadfd
Bishal Dey <bishaldey.in@gmail.com>
2026-08-08 22:19:04 +0530

    some new codes

---
+++ Array/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //declaration of the array
+    int laxman[101];
+
+    int arr[12];//idhar agar[] iske andar value declare nhi kiya toh error aa jaega
+
+    //initialization bolte hai niche walo dono ko0
+  int brr[5] ={1,2,3,4,5};
+  int crr[]={1,2,3,4,};
+//   cout << brr[1]<<endl;
+  for(int i=0;i<=5;i++){
+    cout<<crr[i];
+  }
+
+    return 0;
+}
